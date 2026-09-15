a7971087791dae2a67e0c9f849fbfb414f629a10
Renan Geraldini Leão <renancvr777@gmail.com>
2026-09-15 03:25:05 -0300

    adjusts v10

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,8 +21,8 @@ export default function Footer() {
             <span className="timeline-audio-label">
               {'Renan cantando pra Bela <3'}
             </span>
-            <audio src={"public/audio1.ogg"} controls preload="metadata" aria-label='Áudio' />
-            <audio src={"public/audio2.ogg"} controls preload="metadata" aria-label='Áudio '/>
+            <audio src={"/audio1.ogg"} controls preload="metadata" aria-label='Áudio' />
+            <audio src={"/audio2.ogg"} controls preload="metadata" aria-label='Áudio '/>
           </div>
         </div>
 
